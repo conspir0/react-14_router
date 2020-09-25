5a2380d07b59a7c0dd717831391f3b9902b93801
conspir0 <michalczyk.mat3usz@gmail.com>
2020-09-26 00:17:39 +0200

    Create Nav

--- src/layouts/Nav/Nav.js
+++ src/layouts/Nav/Nav.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Nav.css';
 
+const list = [
+  { name: 'Home', path: '/', exact: true },
+  { name: 'Products', path: '/product', exact: false },
+  { name: 'Contact', path: '/contact', exact: false },
+  { name: 'Login', path: '/login', exact: false },
+]
+
 const Nav = () => {
+  const navList = list.map(item => (
+    <li
+      key={item.name}
+      className="nav-elem"
+    >
+      <NavLink
+        to={item.path}
+        exact={item.exact}
+        activeClassName="nav-link--active"
+        className="nav-link"
+      >
+        {item.name}
+      </NavLink>
+    </li>
+  ));
+
   return (
     <ul className="nav">
-      <li className="nav-elem">Nav</li>
-      <li className="nav-elem">Nav</li>
-      <li className="nav-elem">Nav</li>
+      {navList}
     </ul>
   );
 }
