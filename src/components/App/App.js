b6cba1f80807f0e17f3cd6d51113c35d384e8aec
conspir0 <michalczyk.mat3usz@gmail.com>
2020-09-25 22:05:26 +0200

    Tests with BrowerRouter, Route, Link

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+
+const Home = () => <h1>Home</h1>;
+const News = () => <h1>News</h1>;
+const Contact = () => <h1>Contact</h1>;
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World!
-      </div>
+      <Router>
+        <div>
+          <header>
+            <nav>
+              <ul>
+                <li><Link to="/">Start</Link></li>
+                <li><Link to="/news">News</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+              </ul>
+            </nav>
+          </header>
+          <section>
+            <Route path="/" exact component={Home} />
+            <Route path="/news" component={News} />
+            <Route path="/contact" component={Contact} />
+          </section>
+        </div>
+      </Router>
     );
   }
 }
