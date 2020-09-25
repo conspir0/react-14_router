b21e974ff88de9a18ec1a5bcf0c21a3eabf07bc6
conspir0 <michalczyk.mat3usz@gmail.com>
2020-09-25 23:40:43 +0200

    Add basic view and style

--- src/layouts/App/App.js
+++ src/layouts/App/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Header from './../Header/Header';
+import Nav from './../Nav/Nav';
+import Section from './../Section/Section';
+import Footer from './../Footer/Footer';
 import './App.css';
 
 
 class App extends Component {
   render() {
     return (
-      <div>Hello</div>
+      <Router>
+        <div className="container">
+          <header>
+            <Header />
+          </header>
+          <main>
+            <nav>
+              <Nav />
+            </nav>
+            <section>
+              <Section />
+            </section>
+          </main>
+          <footer>
+            <Footer />
+          </footer>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/layouts/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Nav.css';
+
+const Nav = () => {
+  return (
+    <ul className="nav">
+      <li className="nav-elem">Nav</li>
+      <li className="nav-elem">Nav</li>
+      <li className="nav-elem">Nav</li>
+    </ul>
+  );
+}
+
+export default Nav;
