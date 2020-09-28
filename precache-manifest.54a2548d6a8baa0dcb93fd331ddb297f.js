74c18cb8f6778f285e45a87fb8a6c3c35335803c
conspir0 <michalczyk.mat3usz@gmail.com>
2020-09-29 00:29:05 +0200

    Updates

---
+++ precache-manifest.54a2548d6a8baa0dcb93fd331ddb297f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2545c88321359a45acbca590bca3e0bc",
+    "url": "/react-14_router/static/media/products.2545c883.jpeg"
+  },
+  {
+    "revision": "be84ba9029637ad6196f",
+    "url": "/react-14_router/static/css/main.87d1cec8.chunk.css"
+  },
+  {
+    "revision": "23665cc23ee8f1737cb0",
+    "url": "/react-14_router/static/js/runtime~main.e501bfa6.js"
+  },
+  {
+    "revision": "263e5f1be680fff50738",
+    "url": "/react-14_router/static/js/2.09f5dbca.chunk.js"
+  },
+  {
+    "revision": "e8a9be882726ca610444d9933145e9f0",
+    "url": "/react-14_router/static/media/home.e8a9be88.jpeg"
+  },
+  {
+    "revision": "be84ba9029637ad6196f",
+    "url": "/react-14_router/static/js/main.5fe14b9a.chunk.js"
+  },
+  {
+    "revision": "7e7ab0fd5d2cb018c9490b0d53d3320a",
+    "url": "/react-14_router/static/media/contact.7e7ab0fd.jpeg"
+  },
+  {
+    "revision": "0170e43600892fee9f812b3c94e4b0a7",
+    "url": "/react-14_router/static/media/login.0170e436.jpeg"
+  },
+  {
+    "revision": "b265dc17510684b153e3048af085cec9",
+    "url": "/react-14_router/static/media/admin.b265dc17.jpeg"
+  },
+  {
+    "revision": "94b47e3843f8944e5a1e506a8322c060",
+    "url": "/react-14_router/static/media/error.94b47e38.png"
+  },
+  {
+    "revision": "a1e7a69a4d2d063d0135ae8e8d67d4d8",
+    "url": "/react-14_router/index.html"
+  }
+];
